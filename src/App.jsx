f8c2fa04104a0d288438d2f428e7a0257134cdea
frrann <ada.fran92@gmail.com>
2023-11-09 14:42:27 +0200

    Create a general layout of the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import AppLayout from './ui/AppLayout';
+
 function App() {
-  return <h1>Product feedback app</h1>;
+  return (
+    <AppLayout>
+      <div className="mt-10 rounded-lg bg-neutral-white md:mt-16 xl:mt-0">
+        <div className="px-6 py-11 md:px-[42px] md:pt-[52px]">
+          <p className="text-lg font-bold text-blue-dark md:text-2xl">
+            AppLayout
+          </p>
+        </div>
+      </div>
+    </AppLayout>
+  );
 }
 
 export default App;
