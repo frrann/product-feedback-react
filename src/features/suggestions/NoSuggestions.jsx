13beb02964da57f058ff92f62e47765582f32a6a
frrann <ada.fran92@gmail.com>
2024-01-11 12:03:37 +0200

    Update NoSuggestions page

--- src/features/suggestions/NoSuggestions.jsx
+++ src/features/suggestions/NoSuggestions.jsx
@@ -8,11 +8,13 @@ function NoSuggestions() {
   const navigate = useNavigate();
 
   return (
-    <div className="grid justify-center justify-items-center gap-12 p-10">
+    <div className="grid justify-center justify-items-center gap-6 rounded-lg bg-neutral-white p-10 md:gap-12 md:py-28">
       <img src={IllustrationEmpty} alt="No suggestions yet" />
-      <div className="text-center">
-        <h1 className="text-blue-midnight">There is no feedback yet.</h1>
-        <p className="custom-body-1 text-neutral-grey">
+      <div className="mt-4 text-center md:px-24 lg:px-40">
+        <h3 className="text-blue-midnight md:text-2xl">
+          There is no feedback yet.
+        </h3>
+        <p className="custom-body-3 md:custom-body-1 mt-[14px] font-normal text-neutral-grey">
           Got a suggestion? Found a bug that needs to be squashed? We love
           hearing about new ideas to improve our app.
         </p>
